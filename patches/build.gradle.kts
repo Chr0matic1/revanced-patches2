84d1d2402d1c72a5bfed36c43d8897e14e97d653
Chr0matic1 <101169615+Chr0matic1@users.noreply.github.com>
2024-12-11 23:11:23 +0100

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,13 +1,13 @@
-group = "app.revanced"
+group = "example"
 
 patches {
     about {
         name = "ReVanced Patches template"
         description = "Patches template for ReVanced"
-        source = "git@github.com:revanced/revanced-patches-template.git"
-        author = "ReVanced"
-        contact = "contact@revanced.app"
-        website = "https://revanced.app"
+        source = "git@github.com:Chr0matic1/revanced-patches2.git"
+        author = "Example"
+        contact = "contact@example.url"
+        website = "https://example.url"
         license = "GNU General Public License v3.0"
     }
 }
